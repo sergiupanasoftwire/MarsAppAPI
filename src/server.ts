e6803c9edb1409043be01b06191df2794b61a5dd
Sergiu Pana <sergiu.pana@softwire.com>
2023-07-05 17:52:00 +0300

    Implemented endpoint for fetching data about a specific race

--- src/server.ts
+++ src/server.ts
@@ -9,22 +9,50 @@ const port = 8080;
 const SOL: number = 1000;
 const F1_API_URL: string = 'https://ergast.com/api/f1'
 
-class RaceInfo {
+const Scraper = require('images-scraper');
+
+const google = new Scraper({
+    puppeteer: {
+        headless: true
+    },
+    tbs: {
+        isz: "m"
+    }
+});
+
+const imagesCache: Record<string, string> = {}
+
+ interface RaceInfo {
     round: number;
     raceName: string;
     circuitName: string;
     circuitId: string;
+}
 
-    constructor(round: number, raceName: string, circuitName: string, circuitId: string) {
-        this.round = round;
-        this.raceName = raceName;
-        this.circuitName = circuitName;
-        this.circuitId = circuitId;
-    }
+interface RaceDetails {
+    info: RaceInfo;
+    standings: Result[];
+    circuitImage: string;
+}
+
+interface Result {
+    position: string;
+    points: string;
+    startingPosition: string;
+    driver: Driver;
+}
+
+interface Driver {
+    firstName: string;
+    lastName: string;
+    code: string;
+    number: string;
+    nationality: string;
+    constructor: string;
 }
 
 app.use(express.json());
-app.use(function(req: Request, res: Response, next: NextFunction) {
+app.use(function (req: Request, res: Response, next: NextFunction) {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header(
         'Access-Control-Allow-Headers',
@@ -41,23 +69,73 @@ router.get('/f1/seasons/:seasonYear', async (req: Request, res: Response) => {
     return;
 });
 router.get('/f1/seasons/:seasonYear/rounds/:roundNumber', async (req: Request, res: Response) => {
-    const reqURL = F1_API_URL + `/${req.params.seasonYear}/${req.params.roundNumber}.json`;
+    const reqURL = F1_API_URL + `/${req.params.seasonYear}/${req.params.roundNumber}/results.json`;
     const axiosRes = await axios.get(reqURL).catch((err: any) => res.json(err));
-    res.json(axiosRes.data);
+    const details: RaceDetails = {
+        info: {
+            round: Number(req.params.roundNumber),
+            raceName: '',
+            circuitName: '',
+            circuitId: ''
+        },
+        standings: [],
+        circuitImage: ''
+    };
+
+    axiosRes.data.MRData.RaceTable.Races[0].Results.forEach((result: any )=> {
+            details.standings.push({
+                position: result.position,
+                points: result.points,
+                startingPosition: result.grid,
+                driver: {
+                    firstName: result.Driver.givenName,
+                    lastName: result.Driver.familyName,
+                    code: result.Driver.code,
+                    nationality: result.Driver.nationality,
+                    number: result.Driver.permanentNumber,
+                    constructor: result.Constructor.name
+                }
+            });
+        }
+    );
+
+    details.info.circuitName = axiosRes.data.MRData.RaceTable.Races[0].Circuit.circuitName;
+    details.info.circuitId = axiosRes.data.MRData.RaceTable.Races[0].Circuit.circuitId;
+    details.info.raceName = axiosRes.data.MRData.RaceTable.Races[0].raceName;
+
+    let query = 'formula 1 ' + details.info.circuitName + ' official layout';
+    console.log(query);
+    if (!imagesCache[query]) {
+        const circuitImage = await google.scrape(query, 1);
+
+        imagesCache[query] = circuitImage[0].url;
+    }
+
+    details.circuitImage = imagesCache[query];
+
+    console.log(imagesCache[query]);
+
+    res.json(details);
     return;
 })
 
-router.get('/f1/seasons/:seasonYear/rounds',  async (req: Request, res: Response) => {
+router.get('/f1/seasons/:seasonYear/rounds', async (req: Request, res: Response) => {
     const reqURL = F1_API_URL + `/${req.params.seasonYear}.json`;
     const axiosRes = await axios.get(reqURL).catch((err: any) => res.json(err));
 
     const startRound: number = parseInt(req.query.startRound as string);
     const endRound: number = parseInt(req.query.endRound as string);
 
+
     let racesNames: Array<RaceInfo> = new Array<RaceInfo>();
     axiosRes.data.MRData.RaceTable.Races.forEach((race: any) => {
         racesNames.push(
-            new RaceInfo(race.round, race.raceName, race.Circuit.circuitName, race.Circuit.circuitId)
+            {
+                round: race.round,
+                raceName: race.raceName,
+                circuitName: race.Circuit.circuitName,
+                circuitId: race.Circuit.circuitId
+            },
         )
     });
 
